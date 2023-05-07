d29087364503ddb8e7d9b913c86b59d1db71745d
Vano <vanovasilyan@gmail.com>
2023-05-07 12:49:31 +0400

    add blur effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useGlobalContext } from './context/context';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import './index.css'
 import ListItems from './Components/ListItems/ListItems';
 
 const App = () => {
+  const { isShowSideBar, showSideBar } = useGlobalContext();
   return (
     <div className='main'>
+      {isShowSideBar && <div onClick={showSideBar} className="blur"></div>}
       <Header />
       <Navbar />
       <ListItems />
